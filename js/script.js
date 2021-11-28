2134e0a39fc9adf29a50d19ef062972cf3fb4dad
Alina-Zadorojnaya <alinasmartmebel@gmail.com>
2021-11-28 10:14:10 +0200

    hw-7

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 
+
+
+
+$(function () {
+
+ $('.header__burger').on('click', function(){
+        $('.header__burger,.menu__list').toggleClass('active');
+      });
+});
+    
 var mixer = mixitup ( '.portfolio__container' );
 
- 
